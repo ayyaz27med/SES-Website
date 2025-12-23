81e96c2b952a68c5e14c6bd8eb99deef1cf8b6d8
gautam-ramkabir <gautamramkabir79@gmail.com>
2025-12-23 16:40:55 +0530

    show notify me button when user is logged in

--- components/productCards/ProductCard.jsx
+++ components/productCards/ProductCard.jsx
@@ -164,7 +164,7 @@ export default function ProductCard({
             <span className="tooltip">Quick View</span>
           </a>
         </div>
-        {product.stock_status === 'out_of_stock' && (
+        {isAuthenticated && product.stock_status === 'out_of_stock' && (
           <div className="list-btn-main">
             <a
               className="btn-main-product"
